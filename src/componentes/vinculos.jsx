40c24628528ad46397263af6ae3123f66a7df840
Nicolas Chavez <90515899+GitJN-1@users.noreply.github.com>
2021-10-08 13:32:39 -0500

    Delete src directory

--- src/componentes/vinculos.jsx
+++
@@ -1,5 +0,0 @@
-function vinculos({pagina}) {
-    return <a className="vinculo" href="./inicio.html">{pagina}</a>;
-}
-
-export default vinculos;
--- src/paginas/index.jsx
+++
@@ -1,7 +0,0 @@
-import vinculos from "../componentes/vinculos"
-
-function Index() {
-    return <div>Hola mundo</div>;
-}
-
-export default Index;
