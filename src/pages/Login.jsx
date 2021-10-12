bcbb3a985e4a2fa27301488dff5001351cd80ab4
Juan Jose <juanjose180201@hotmail.com>
2021-10-11 20:48:08 -0500

    Agrego primera parte de tabla dinamica

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,13 @@
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../styles/login.css";
 import Footer from '../components/Footer.jsx';
 import { Link } from "react-router-dom";
 
-function Login () {
+const Login = () => {
+    
+    const [variableEstado, setVariableEstado] = useState("");
+
     return (
         <>
             <div className="container">
@@ -12,15 +15,23 @@ function Login () {
                     <div className="login">
                         <img className="avatar" src="https://github.com/Runteams-MisionTIC/Proyecto_web/blob/main/media/logo.png?raw=true" alt="logo" />
                         <h2>Incio de sesión</h2>
-                        <form action="">
-                            <input type="text" name="username" placeholder="Usuario" class="username"/>
+                        <form>
+                            
+                            <input onChange={(e) => {
+                                setVariableEstado(e.target.value)
+                            }} type="text" name="username" 
+                                placeholder="Usuario" class="username"
+                            />
                             <input type="password" name="passaword" placeholder="Contraseña" class="passaword"/>
-                            <Link to="/Ventas">
-                                <input id="submit" type="submit" value="Siguiente" className="submit" />
-                            </Link>
+                            
+                            <button type="button" onClick={(e) => {
+                                console.log("valor variable = ", variableEstado);
+                            }} id="submit" className="submit">Seguir
+                            </button>
+
                         </form>
                         <p>¿Ha olvidado su usuario o contraseña?</p>
-                        <button className="icono"><i class="fab fa-google"></i>Continuar con Google</button>
+                        <button className="icono"><i className="fab fa-google"></i>Continuar con Google</button>
                     </div>
                 </div>
             </div>
--- src/pages/Ventas.jsx
+++ src/pages/Ventas.jsx
@@ -1,46 +1,69 @@
+import React, {useState, useEffect} from 'react';
 import '../styles/ventas.css';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 const Ventas = () => {
+
+    const [mostrarTabla, setMostrarTabla] = useState(true);
+    const [textoBoton, setTextoBoton] = useState("Nueva venta")
+
+    useEffect(() => {
+        if (mostrarTabla == false) {
+            setTextoBoton("Mostrar tabla")
+        }else{
+            setTextoBoton("Nueva venta")
+        }
+    })
+
     return (
         <div className="Ventas">
             <Header/>
             <section>
-                <button className="ventas">Agregar</button>
-                <button className="ventas">Eliminar</button>
-                <button className="ventas">Procesar</button>
-                <button className="ventas">Buscar</button>
-                <button className="ventas">Reportes</button>
-                <button className="ventas">Editar</button>
+                <button  onClick = {() => {setMostrarTabla(!mostrarTabla)}} className="ventas">{textoBoton}</button>
+                {mostrarTabla ? <TablaVentas/> : <FormularioVentas/>}
             </section>
-            <section id="contenedor-principal">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Nombre cliente</th> <th>Documento</th> <th>Telefono</th> <th>Producto</th> <th>Cantidad</th> <th>Valor unitario</th> <th>Valor total</th> <th>ID Venta</th>
-                        </tr>
-                    </thead>
-                    <tr>
-                        <td>Yinare</td> <td>1010101010</td> <td>3110000000</td> <td>Zapatos</td> <td>5</td> <td>600</td> <td>$3.000</td> <td>#00000000</td>
-                    </tr>
-                    <tr>
-                        <td>Sebastian</td> <td>1010101010</td> <td>3110000000</td> <td>Camisetas</td> <td>5</td>  <td>600</td> <td>$3.000</td> <td>#00000000</td>
-                    </tr>
-                    <tr>
-                        <td>Osbaldo</td> <td>1010101010</td> <td>3110000000</td> <td>Pantalones</td> <td>5</td>  <td>600</td> <td>$3.000</td> <td>#00000000</td>
-                    </tr>
-                    <tr>
-                        <td>Nicolas</td> <td>1010101010</td> <td>3110000000</td> <td>Gorros</td> <td>5</td>  <td>600</td> <td>$3.000</td> <td>#00000000</td>
-                    </tr>
-                    <tr>
-                        <td>Juan</td> <td>1010101010</td> <td>3110000000</td> <td>Chaquetas</td> <td>5</td>  <td>600</td> <td>$3.000</td> <td>#00000000</td>
-                    </tr>
-                </table>
-            </section>
-
             <Footer/>
         </div>
+    );
+}
+
+const FormularioVentas = () => {
+    return (
+        <span className='contenedor-principal'>BABA</span>
+    );
+}
+
+
+const TablaVentas = () => {
+    return(
+        <section className="contenedor-principal">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre cliente</th> <th>Documento</th> <th>Telefono</th> <th>Producto</th> <th>Cantidad</th> <th>Valor unitario</th> <th>Valor total</th> <th>ID Venta</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>Yinare</td> <td>1010101010</td> <td>3110000000</td> <td>Zapatos</td> <td>5</td> <td>600</td> <td>$3.000</td> <td>#00000000</td>
+                </tr>
+                <tr>
+                    <td>Sebastian</td> <td>1010101010</td> <td>3110000000</td> <td>Camisetas</td> <td>5</td>  <td>600</td> <td>$3.000</td> <td>#00000000</td>
+                </tr>
+                <tr>
+                    <td>Osbaldo</td> <td>1010101010</td> <td>3110000000</td> <td>Pantalones</td> <td>5</td>  <td>600</td> <td>$3.000</td> <td>#00000000</td>
+                </tr>
+                <tr>
+                    <td>Nicolas</td> <td>1010101010</td> <td>3110000000</td> <td>Gorros</td> <td>5</td>  <td>600</td> <td>$3.000</td> <td>#00000000</td>
+                </tr>
+                <tr>
+                    <td>Juan</td> <td>1010101010</td> <td>3110000000</td> <td>Chaquetas</td> <td>5</td>  <td>600</td> <td>$3.000</td> <td>#00000000</td>
+                </tr>
+            </table>
+            <button className="ventas">Procesar</button>
+            <button className="ventas">Buscar</button>
+            <button className="ventas">Reportes</button>
+        </section>
     );
 }
 
