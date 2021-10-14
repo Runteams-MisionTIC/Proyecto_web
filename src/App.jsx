7f06cadb6482dfbc65d874393114a6e0f3a79e35
Nicolas Chavez <90515899+GitJN-1@users.noreply.github.com>
2021-10-13 19:58:12 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Index from './pages/Index.jsx'
 import Ventas from './pages/Ventas.jsx'
-import Productos from './pages/Productos.jsx'
+import Productos from './pages/Productos.jsx';
+import Login from './pages/Login.jsx';
+import Credenciales from './pages/Credenciales.jsx';
+import Administracion from './pages/AdminUsuarios.jsx';
 import './styles/App.css';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
   } from "react-router-dom";
 
 function App() {
@@ -14,18 +16,26 @@ function App() {
     <div className="App">
         <Router>
             <Switch>
-                <Route path='/Ventas'>
-                    <Ventas/>
+                <Route path='/Login'>
+                  <Login/>
+                </Route>
+                <Route path='/Credenciales'>
+                  <Credenciales/>
+                </Route>
+                <Route path='/Administracion'>
+                  <Administracion/>
                 </Route>
                 <Route path='/Productos'>
-                    <Productos/>
+                  <Productos/>
+                </Route>
+                <Route path='/Ventas'>
+                  <Ventas/>
                 </Route>
                 <Route path='/'>
-                    <Index/>
+                  <Index/>
                 </Route>
             </Switch>
-        </Router>
-        
+        </Router> 
     </div>
   );
 }
