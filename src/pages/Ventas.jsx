7daf443d61557bf04aafcc5ea496dec8ee4ff299
Juan Jose <juanjose180201@hotmail.com>
2021-10-20 16:51:36 -0500

    Arreglo problema de edicion

--- src/pages/Ventas.jsx
+++ src/pages/Ventas.jsx
@@ -12,57 +12,48 @@ import { nanoid } from 'nanoid';
 const Ventas = () => {
     
     const [mostrarTabla, setMostrarTabla] = useState(true);
-    const [textoBoton, setTextoBoton] = useState("Nueva venta")
-    const [ventas, setVentas] = useState([])
-    const [hacerConsulta, setHacerConsulta] = useState(true)
+    const [textoBoton, setTextoBoton] = useState("Nueva venta");
+    const [ventas, setVentas] = useState([]);
+    const [ejecutarConsulta, setEjecutarConsulta] = useState(true);
+
+    const obtenerVentas = async () => {
+        const options = { method: 'GET', url: 'http://localhost:2999/ventas/' };
+        await axios.request(options).then(function (response) {
+            setVentas(response.data);
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+        setEjecutarConsulta(false);
+    };
+
+    useEffect(()=>{
+        if (ejecutarConsulta) {
+            obtenerVentas();
+        }
+    },[ejecutarConsulta]);
 
     useEffect(() => {
-        const obtenerVentas = async () => {
-            const options = {method: 'GET', url: 'http://localhost:2999/ventas'}
-            await axios.request(options).then(function(response){
-                setVentas(response.data)
-            })
-            .catch(function(error){
-                console.log(error)
-            });
-            if (hacerConsulta) {
-                obtenerVentas()
-                setHacerConsulta(false)
-            }
+        if (mostrarTabla) {
+          setEjecutarConsulta(true);
         }
-    },[hacerConsulta])
+    }, [mostrarTabla]);
 
     useEffect(() => {
-        const obtenerVentas = async () => {
-            const options = {method: 'GET', url: 'http://localhost:2999/ventas'}
-            await axios.request(options).then(function(response){
-                setVentas(response.data)
-            })
-            .catch(function(error){
-                console.log(error)
-            });
+        if (mostrarTabla) {
+          setTextoBoton('Nueva venta');
+        } else {
+          setTextoBoton('Mostrar ventas');
         }
-        if(mostrarTabla){
-            obtenerVentas()
-        }
-    },[]);
-
-    useEffect(() => {
-        if (mostrarTabla === false) {
-            setTextoBoton("Mostrar tabla");
-            setHacerConsulta(true)
-        }else{
-            setTextoBoton("Nueva venta");
-        }
-    },[mostrarTabla]);
+    }, [mostrarTabla]);
 
     return (
         <div className="Ventas">
             <Header/>
             <section>
                 <button  onClick = {() => {setMostrarTabla(!mostrarTabla)}} className="ventas">{textoBoton}</button>
-                {mostrarTabla ? <TablaVentas  listaVentas = {ventas}/> : 
-                <FormularioVentas cambiarATabla={setMostrarTabla} listaVentas={ventas} ingresarInformacionVenta={setVentas}/>}
+                {mostrarTabla ? ( <TablaVentas  listaVentas = {ventas} setEjecutarConsulta={setEjecutarConsulta} />) : 
+                (<FormularioVentas setMostrarTabla={setMostrarTabla} listaVentas={ventas} setVentas={setVentas}/>)}
                 <ToastContainer position="bottom-center" autoClose={5000} />
             </section>
             <Footer/>
@@ -70,12 +61,145 @@ const Ventas = () => {
     );
 }
 
-const FormularioVentas = ({cambiarATabla, listaVentas, ingresarInformacionVenta}) => {
+const TablaVentas = ({listaVentas, setEjecutarConsulta}) => {
+
+    return(
+        <section className="contenedor-principal">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre cliente</th>
+                        <th>Documento</th>
+                        <th>Telefono</th>
+                        <th>Producto</th>
+                        <th>Cantidad</th>
+                        <th>Valor unitario</th>
+                        <th>Valor total</th>
+                        <th>ID Venta</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listaVentas.map((ventas)=>{
+                        return <FilaVentas key={nanoid()} ventas={ventas} setEjecutarConsulta={setEjecutarConsulta} />
+                    })}
+                </tbody>
+            </table>
+            <button className="ventas">Procesar</button>
+            <button className="ventas">Buscar</button>
+            <button className="ventas">Reportes</button>
+        </section>
+    );
+}
+
+const FilaVentas = ({ventas, setEjecutarConsulta}) => {
+
+    const [editar, setEditar] = useState(false);
+
+    const [infoNuevaVenta, setInfoNuevaVenta] = useState({
+        nombre: ventas.nombre,
+        documento: ventas.documento,
+        telefono: ventas.telefono,
+        producto: ventas.producto,
+        cantidad: ventas.cantidad
+    });
+
+    const actualizarVenta = async () => {
+        
+        const options = {
+            method: 'PATCH',
+            url: `http://localhost:2999/ventas/${ventas._id}/`,
+            headers: {'content-type':'application/json'},
+            data: {...infoNuevaVenta },
+        }
+        await axios.request(options).then(function(response){
+            console.log(response.data);
+            setEditar(false);
+            setEjecutarConsulta(true);
+            toast.success('La venta ha sido editada con exito');
+        })
+        .catch(function(error){
+            console.log(error)
+            toast.error('La venta no pudo ser editada');
+        })
+    }
+
+    const eliminarVenta = async () => {
+        const options = {
+            method: 'DELETE',
+            url: `http://localhost:2999/ventas/${ventas._id}/`,
+            headers: {'content-type':'application/json'},
+            data: {id: ventas._id},
+        }
+        await axios.request(options).then(function(response){
+            console.log(response.data)
+            setEjecutarConsulta(true)
+            toast.success('La venta ha sido agregada con exito');
+        })
+        .catch(function(error){
+            console.log(error)
+            toast.error('La venta no pudo ser agregada');
+        });
+    }
+
+    return(
+        <tr>
+            {editar ? (
+                <>
+                    <td><input className="inputsTabla" type="text" 
+                        value={infoNuevaVenta.nombre} 
+                        onChange={(e)=>setInfoNuevaVenta({...infoNuevaVenta, nombre: e.target.value})} />
+                    </td>
+                    <td><input className="inputsTabla" type="text" 
+                        value={infoNuevaVenta.documento} 
+                        onChange={(e)=>setInfoNuevaVenta({...infoNuevaVenta, documento: e.target.value})} />
+                    </td>
+                    <td><input className="inputsTabla" type="text" 
+                        value={infoNuevaVenta.telefono} 
+                        onChange={(e)=>setInfoNuevaVenta({...infoNuevaVenta, telefono: e.target.value})} />
+                    </td>
+                    <td><input className="inputsTabla" type="text" 
+                        value={infoNuevaVenta.producto} 
+                        onChange={(e)=>setInfoNuevaVenta({...infoNuevaVenta, producto: e.target.value})} />
+                    </td>
+                    <td><input className="inputsTabla" type="text" 
+                        value={infoNuevaVenta.cantidad} 
+                        onChange={(e)=>setInfoNuevaVenta({...infoNuevaVenta, cantidad: e.target.value})} />
+                    </td>
+                </>
+             )  : (
+                    <>
+                        <td>{ventas.nombre}</td>
+                        <td>{ventas.documento}</td>
+                        <td>{ventas.telefono}</td>
+                        <td>{ventas.producto}</td>
+                        <td>{ventas.cantidad}</td>
+                    </>
+                )
+            }
+
+            <td>600</td>
+            <td>$3.000</td>
+            <td>#00000000</td>
+            <td>
+                <div className="iconos">
+                    {editar ? (
+                        <i onClick={() => {actualizarVenta()}} id="check" className="fas fa-check"></i>
+                    ):(
+                        <i onClick={() => {setEditar(!editar)}} id="pencil" className="fas fa-pencil-alt"></i>
+                    )}
+                    <i onClick={() => {eliminarVenta()}} id="trashCan" className="fas fa-trash"></i>
+                </div>
+            </td>
+        </tr>
+    );
+}
+
+const FormularioVentas = ({setMostrarTabla, listaVentas, setVentas}) => {
 
     const form = useRef(null)     
 
     const enviarBackend = async (e) => {
-        
         e.preventDefault();
         const fd = new FormData(form.current);
         const nuevaVenta = {};
@@ -83,7 +207,7 @@ const FormularioVentas = ({cambiarATabla, listaVentas, ingresarInformacionVenta}
 
         const options = {
             method: 'POST',
-            url: 'http://localhost:2999/ventas/nueva',
+            url: 'http://localhost:2999/ventas/',
             headers: {'content-type':'application/json'},
             data: {nombre:nuevaVenta.nombre, documento:nuevaVenta.documento, 
                    telefono:nuevaVenta.telefono, producto:nuevaVenta.producto, 
@@ -98,8 +222,7 @@ const FormularioVentas = ({cambiarATabla, listaVentas, ingresarInformacionVenta}
             toast.error('La venta no pudo ser agregada');
         })
 
-        ingresarInformacionVenta([...listaVentas, nuevaVenta])
-        cambiarATabla(true);
+        setMostrarTabla(true);
     }
 
     return (
@@ -131,126 +254,6 @@ const FormularioVentas = ({cambiarATabla, listaVentas, ingresarInformacionVenta}
                 />
             <button type='submit' id="saveButton">Guardar</button>
         </form>
-    );
-}
-
-const TablaVentas = ({listaVentas, setHacerConsulta}) => {
-
-    return(
-        <section className="contenedor-principal">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nombre cliente</th>
-                        <th>Documento</th>
-                        <th>Telefono</th>
-                        <th>Producto</th>
-                        <th>Cantidad</th>
-                        <th>Valor unitario</th>
-                        <th>Valor total</th>
-                        <th>ID Venta</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {listaVentas.map((ventas)=>{
-                        return <FilaVentas key={nanoid()} ventas={ventas} setHacerConsulta={setHacerConsulta} />
-                    })}
-                </tbody>
-            </table>
-            <button className="ventas">Procesar</button>
-            <button className="ventas">Buscar</button>
-            <button className="ventas">Reportes</button>
-        </section>
-    );
-}
-
-const FilaVentas = ({ventas, setHacerConsulta}) => {
-
-    const [editar, setEditar] = useState(false);
-
-    const [infoNuevaVenta, setInfoNuevaVenta] = useState({
-        nombre: ventas.nombre,
-        documento: ventas.documento,
-        telefono: ventas.telefono,
-        producto: ventas.producto,
-        cantidad: ventas.cantidad
-    })
-
-    const actualizarVenta = async () => {
-        //enviar la info al backend
-        
-        const options = {
-            method: 'PATCH',
-            url: 'http://localhost:2999/ventas/editar',
-            headers: {'content-type':'application/json'},
-            data: {...infoNuevaVenta, id:ventas._id}
-        }
-        await axios.request(options).then(function(response){
-            console.log(response.data)
-            setEditar(false)
-            setHacerConsulta(true)
-            toast.success('La venta ha sido editada con exito');
-        })
-        .catch(function(error){
-            console.log(error)
-            toast.error('La venta no pudo ser editada');
-        })
-    }
-
-    const eliminarVenta = async () => {
-        const options = {
-            method: 'DELETE',
-            url: 'http://localhost:2999/ventas/eliminar',
-            headers: {'content-type':'application/json'},
-            data: {id:ventas._id}
-        }
-        await axios.request(options).then(function(response){
-            console.log(response.data)
-            setHacerConsulta(true)
-            toast.success('La venta ha sido agregada con exito');
-        })
-        .catch(function(error){
-            console.log(error)
-            toast.error('La venta no pudo ser agregada');
-        })
-    }
-
-    return(
-        <tr>
-            {editar ? (
-                <>
-                    <td><input name='nombre' className="inputsTabla" type="text" value={infoNuevaVenta.nombre} onChange={e=>{setInfoNuevaVenta({...infoNuevaVenta,nombre: e.target.nombre})}} /></td>
-                    <td><input name='documento' className="inputsTabla" type="text" value={infoNuevaVenta.documento} onChange={e=>{setInfoNuevaVenta({...infoNuevaVenta,documento: e.target.documento})}} /></td>
-                    <td><input name='telefono' className="inputsTabla" type="text" value={infoNuevaVenta.telefono} onChange={e=>{setInfoNuevaVenta({...infoNuevaVenta,telefono: e.target.telefono})}} /></td>
-                    <td><input name='producto' className="inputsTabla" type="text" value={infoNuevaVenta.producto} onChange={e=>{setInfoNuevaVenta({...infoNuevaVenta,producto: e.target.producto})}} /></td>
-                    <td><input name='cantidad' className="inputsTabla" type="text" value={infoNuevaVenta.cantidad} onChange={e=>{setInfoNuevaVenta({...infoNuevaVenta,cantidad: e.target.cantidad})}} /></td>
-                </>
-             )  : (
-                    <>
-                        <td>{ventas.nombre}</td>
-                        <td>{ventas.documento}</td>
-                        <td>{ventas.telefono}</td>
-                        <td>{ventas.producto}</td>
-                        <td>{ventas.cantidad}</td>
-                    </>
-                )
-            }
-
-            <td>600</td>
-            <td>$3.000</td>
-            <td>#00000000</td>
-            <td>
-                <div className="iconos">
-                    {editar ? (
-                        <i onClick={() => {actualizarVenta(); setEditar(false)}} id="check" className="fas fa-check"></i>
-                    ):(
-                        <i onClick={() => {setEditar(!editar)}} id="pencil" className="fas fa-pencil-alt"></i>
-                    )}
-                    <i onClick={() => {eliminarVenta()}} id="trashCan" className="fas fa-trash"></i>
-                </div>
-            </td>
-        </tr>
     );
 }
 
