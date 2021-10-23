731e9e30f42f810021c622bfcb84106b51523a66
Nicolas Chavez <90515899+GitJN-1@users.noreply.github.com>
2021-10-22 22:59:01 -0500

    ajustar codigo credenciales

--- src/pages/Credenciales.jsx
+++ src/pages/Credenciales.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 import '../styles/credenciales.css';
 import { Link } from "react-router-dom";
 
@@ -7,17 +8,7 @@ import { Link } from "react-router-dom";
 function Credenciales() {
     return(
         <div className="Credenciales">
-            <header>
-                <Link to='/'>
-                    <img className="icono" src="https://github.com/Runteams-MisionTIC/Proyecto_web/blob/main/media/logo.png?raw=true" alt="" />
-                </Link>
-                <nav className="navbar">
-                    <Link to="/">Inicio</Link>
-                    <Link to="/Ventas">Ventas</Link>
-                    <Link to="/Productos">Productos</Link>
-                    <Link to="/Administracion">Administración</Link>
-                </nav>
-            </header>
+            <Header/>
             <div className="Container-admin">
                 <div className="admin">
                     <h6>Seguridad de Runteams</h6>
